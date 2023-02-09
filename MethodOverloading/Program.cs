130d9f3d71c2331498a71038ab33988cf702d8b3
Kalkidan Shibeshi <kkalitemesgen@gmail.com>
2023-02-09 12:06:57 -0500

    new commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,41 @@
 {
     public class Program
     {
+       public static int Add (int num1, int num2)
+        {
+            return num1 + num2;
+        }
+       public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+        public static string Add(int num1, int num2, bool Mybool)
+        {
+            var sum = num1 + num2;
+            if(Mybool == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString() ; 
+            }
+
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var ans = Add(1, 2, true);
+            Console.WriteLine(Add(1, 2));
+            Console.WriteLine(Add(2.3m, 3.4m));
+            Console.WriteLine(ans);
+            Console.WriteLine(Add(2, 3, true));
+             
+
         }
+        
+
+
     }
 }
